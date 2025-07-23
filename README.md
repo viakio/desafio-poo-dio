2551a59e52872a96c936cc55ccb7410430983531
viakio <victorakio10@gmail.com>
2025-07-23 14:06:44 -0300

    fixing paths and implementing conteudo, curso and mentoria

---
+++ README.md
@@ -0,0 +1,17 @@
+# Projeto usando POO da DIO / DIO's project using OOP
+
+## Objetivo / Objective
+
+Nesse desafio feito pela DIO o objetivo é abstrair um bootcamp utilizando os conhecimentos de POO aprendidos durante o curso até o momento.  
+
+In this challenge made by DIO the objective is to abstract a bootcamp utilizing the knowledge of OOP learned during the course until now.
+
+## Conceitos principais / Main concepts
+
+A ideia de abstrair um bootcamp é dividi-lo em aulas que representem as principais características que podem ser encontradas em qualquer curso do gênero.
+
+The idea of abstracting a bootcamp is to divide it into classes that represent the main characteristics that can be found in any course of the genre.
+
+## Autor
+
+Victor Akio Hirai
---
+++ src/App.java
@@ -0,0 +1,29 @@
+import br.com.dio.desafio.dominio.Conteudo;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Curso de Java para iniciantes");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("Curso de Python para iniciantes");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Mentoria para tirar dúvidas sobre Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
